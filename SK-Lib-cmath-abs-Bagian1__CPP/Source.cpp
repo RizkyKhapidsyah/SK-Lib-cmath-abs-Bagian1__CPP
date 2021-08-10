7214ba8d5f76c7eef826f046f3bb90ff7b209310
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-10 20:58:41 +0700

    Add project files.

---
+++ SK-Lib-cmath-abs-Bagian1__CPP/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>     // std::cout
+#include <cmath>        // std::abs
+#include <conio.h>
+
+/*
+	Source by CPlusPlus
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+int main() {
+	std::cout << "abs (3.1416) = " << std::abs(3.1416) << '\n';
+	std::cout << "abs (-10.6)  = " << std::abs(-10.6) << '\n';
+
+	_getch();
+	return 0;
+}
